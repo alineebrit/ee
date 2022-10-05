7575b80988f9af00a965062b4daebce657bce7ba
120210474 ALINE DE BRITO DAS NEVES <aline.neves@ccc.ufcg.edu.brgit config --global user.email aline.neves@ccc.ufcg.edu.brgit config --global user.name alineebrit>
2022-10-05 11:41:20 -0300

    atividade

--- src/Fachadas/FachadaLote.java
+++ src/Fachadas/FachadaLote.java
@@ -1,5 +1,26 @@
 package Fachadas;
+import java.util.List;
+
+import Class.Lote;
+import Class.Produto;
+import Servicos.LoteServico;
+
 
 public class FachadaLote {
+
+	LoteServico servico;
+
+	public FachadaLote() {
+		this.servico = new LoteServico();
+	}
+	
+	public void adicionaLote(int qtd, String dataV, Produto prod) {
+		this.servico.adicionaLote( qtd, dataV,  prod);
+	}
+	
+	public List<Lote> listaLotes(){
+		return this.servico.listaLotes();
+	}
+}
 
 }
--- src/Fachadas/FachadaProduto.java
+++ src/Fachadas/FachadaProduto.java
@@ -1,5 +1,25 @@
 package Fachadas;
 
-public class FachadaProduto {
+import java.util.List;
 
+
+public class FachadaProduto {
+	
+
+	public class ProdutoFachada {
+
+		ProdutoService service;
+
+		public ProdutoFachada() {
+			this.service = new ProdutoService();
+		}
+		
+		public void adicionaProduto(String nome, String fabricante, double peso) {
+			this.service.adicionaProduto(nome, fabricante, peso);
+		}
+		
+		public List<Produto> listaProdutos(){
+			return this.service.listaProdutos();
+		}
+	}
 }
